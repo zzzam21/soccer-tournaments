01ef0936a312f33fcbc611e9997d915d572145f6
zzzam21 <jonas01311@gmail.com>
2026-05-05 08:30:28 -0500

    feat: Relation

--- django_project/player/models.py
+++ django_project/player/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+from teams.models import Team
+
 # Create your models here.
 class Player(models.Model):
     name = models.CharField(max_length=100)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='players',  default=None)
 
     def __str__(self):
         return self.name
