6066e847e163503845d7417e9453a859f99ea1f0
Alexyr28 <alexyr24@gmail.com>
2026-05-18 14:28:54 -0500

    feat: Seccion Torneos

--- frontend/src/app/features/landing/landing.ts
+++ frontend/src/app/features/landing/landing.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { toSignal } from '@angular/core/rxjs-interop';
@@ -6,6 +6,8 @@ import { toSignal } from '@angular/core/rxjs-interop';
 import { NavbarComponent } from '../../layouts/navbar/navbar.component';
 import { FooterComponent } from '../../layouts/footer/footer.component';
 import { CountUpComponent } from '../../shared/components/count-up/count-up.component';
+import { TournamentsService } from '../../../client/services/tournaments.service';
+import type { Tournament } from '../../../client/models';
 import { StatsActions } from '../../store/Stats/stats.actions';
 import { selectStatsData, selectStatsLoading } from '../../store/Stats/stats.selectors';
 
@@ -18,11 +20,21 @@ import { selectStatsData, selectStatsLoading } from '../../store/Stats/stats.sel
 })
 export class Landing implements OnInit {
   private store = inject(Store);
+  private tournamentsService = inject(TournamentsService);
 
   stats = toSignal(this.store.select(selectStatsData), { initialValue: null });
   loading = toSignal(this.store.select(selectStatsLoading), { initialValue: false });
+  tournaments = signal<Tournament[]>([]);
+  tournamentsLoading = signal(true);
 
   ngOnInit(): void {
     this.store.dispatch(StatsActions.load());
+    this.tournamentsService.tournamentsList().subscribe({
+      next: (list) => {
+        this.tournaments.set(list);
+        this.tournamentsLoading.set(false);
+      },
+      error: () => this.tournamentsLoading.set(false),
+    });
   }
 }
